84d7e0d1c143db935bc3eb4e847a333745ec957d
Eric West <esw9999@gmail.com>
2014-11-06 21:30:54 -0600

    Add example to docs.

--- lib/spellbook/completions.ex
+++ lib/spellbook/completions.ex
@@ -1,5 +1,16 @@
 defmodule Spellbook.Completions do
 
+  @doc """
+  Takes the code from the editor and generates completions.
+  
+  ## Example
+    iex(1)> Spellbook.Completions.complete('IO.')
+    {:yes, [],
+     ['binread/2', 'binstream/2', 'binwrite/2', 'chardata_to_string/1', 'getn/3',
+      'getn/2', 'gets/2', 'inspect/3', 'inspect/2', 'iodata_length/1',
+      'iodata_to_binary/1', 'puts/2', 'read/2', 'stream/2', 'write/2', 'ANSI']}
+
+  """
   def complete(code) do
     Enum.reverse(code)
     |> IEx.Autocomplete.expand
